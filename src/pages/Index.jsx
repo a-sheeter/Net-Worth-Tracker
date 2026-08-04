b8fc199aa111c8a04ac883b2145e2cfab826cdbf
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-08-04 14:23:30 -0400

    dashboard

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,70 @@
-import { useState } from "react";
+// react
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { supabase } from "../utils/supabase";
+// hooks
+import useAccounts from "../hooks/useAccounts";
+import useUser from "../hooks/useUser";
 
+// components
 import NavBar from "../components/NavBar";
 
+//utils
+import { formatCurrency } from "../utils/formatters";
+import { totalBalanceByType, totalNetWorth } from "../utils/calculations";
+import { supabase } from "../utils/supabase";
+
 export default function Index() {
+    const today = new Date();
+    const { user } = useUser();
+
+    /* --- effects --- */
+    useEffect(() => {
+        getProfile();
+    }, [user]);
+
+    /* --- state --- */
+    const { accounts, handleDeleteAccount } = useAccounts();
+    const [profile, setProfile] = useState(null);
+
+    /* --- function --- */
+    async function getProfile() {
+        if (!user) return;
+
+        const { data, error } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("id", user.id)
+            .single();
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        setProfile(data);
+        console.log(data);
+    }
+
+    /* --- name --- */
+    const firstName = profile.name.split(" ")[0];
 
     /* --- Render --- */
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <h1>NET WORTH TRACKER</h1>
+            <p>Hello, {firstName}!</p>
+            <p>Today is: {today.toLocaleDateString()}</p>
+
+            <p>Net Worth: {formatCurrency(totalNetWorth(accounts))}
+            </p>
             <button><Link to="/update-networth">Update Net Worth</Link></button>
+            <button><Link to="/networth-history">Net Worth History</Link></button>
+            <p>Assets: {formatCurrency(totalBalanceByType(accounts, "asset"))}
+            </p><button><Link to="/assets">View details</Link></button>
+            <p>Assets: {formatCurrency(totalBalanceByType(accounts, "liability"))}
+            </p><button><Link to="/liabilities">View details</Link></button>
         </>
     )
 }
--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -1,7 +1,20 @@
+export function totalBalance(accounts) {
+    return accounts.reduce((sum, account) => {
+        return sum + account.balance;
+    }, 0);
+}
+
 export function totalBalanceByType(accounts, type) {
     return accounts.reduce((sum, account) => {
         return account.balance_type === type
             ? sum + account.balance
             : sum;
     }, 0);
+}
+
+export function totalNetWorth(accounts) {
+    const assets = totalBalanceByType(accounts, "asset");
+    const liabilities = totalBalanceByType(accounts, "liability");
+
+    return assets - liabilities;
 }
