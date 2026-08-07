c5b17776b9194dc1e68514fecce1b093e15cc303
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-08-07 14:46:58 -0400

    styling

--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { supabase } from "../utils/supabase";
+import Button from "../components/Button";
 
 export default function Login() {
     /* --- Effect --- */
@@ -29,40 +30,45 @@ export default function Login() {
             password: password,
         })
 
-    if (error) {
-        setErrorMessage(error.message);
-        console.log(error.message);
-    } else {
-        console.log("Successfully logged in!");
-    }
+        if (error) {
+            setErrorMessage(error.message);
+            console.log(error.message);
+        } else {
+            console.log("Successfully logged in!");
+        }
 
     }
 
     /* --- Render --- */
-    return(
-        <>
-            <form onSubmit={handleLogin}>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    required
-                    autoComplete={email}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor="password">Password</label>
-                <input 
-                    id="password"
-                    type="password"
-                    required
-                    autoComplete={password}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Log In</button>
-            </form>
-            <p className="error-text">{errorMessage}</p>
-        </>
+    return (
+        <div className="center-all">
+            <div className="small-max-width">
+                <h4 className="text-align-center l-text">Welcome to the</h4>
+                <h1 className="green-text text-align-center xl-text">Net Worth<br /> Tracker</h1>
+                <form onSubmit={handleLogin}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        required
+                        autoComplete={email}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        required
+                        autoComplete={password}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <p className="text-align-center">Don't have an account? <Link to="/register">Register Now</Link></p>
+                    <Button type="submit" className="green-btn">Log In</Button>
+                </form>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+        </div>
     )
 }
