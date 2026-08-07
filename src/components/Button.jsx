a769135a645171099f118b0fcce3abd6d5281272
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-08-07 14:46:53 -0400

    button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({type, className, children}) {
+    return (
+        <>
+            <button type={type} className={className}>{children}</button>
+        </>
+    )
+}
